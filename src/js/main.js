cf679760b9d876465594f0198e65df08d28301c0
Fabio Albertin <albertin@gmail.com>
2017-08-28 23:49:52 +0100

    Updates to main

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,30 @@
 require('../css/main.scss');
+import Game from './game';
 
-import print from './module';
+const DEBUG = true;
 
-print('it works well!');
+const gameModes = document.querySelectorAll('nav ul li a');
+const startButton = document.querySelectorAll('button[name=start]')[0];
+
+let activeMode = 0;
+
+function handleModeEvent(link, ev) {
+  ev.preventDefault();
+  if (DEBUG) {
+    console.log(ev.srcElement.dataset.type);
+  }
+
+  activeMode = ev.srcElement.dataset.type;
+}
+
+gameModes.forEach((link) => {
+  link.addEventListener('click', (ev) => {
+    handleModeEvent(link, ev);
+  });
+});
+
+startButton.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  let currentGame = new Game(activeMode);
+  currentGame.start();
+});
