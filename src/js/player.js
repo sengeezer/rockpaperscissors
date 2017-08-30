3986865e8b09040f67365cc68b3fb290475510bf
Fabio Albertin <albertin@gmail.com>
2017-08-30 23:33:28 +0100

    Tied up two loose ends

--- src/js/player.js
+++ src/js/player.js
@@ -5,9 +5,9 @@ class Player {
 
   takeTurn() {
     if (this.type === 'human') {
-
+      // TODO: To implement
     } else if (this.type === 'machine') {
-
+      // TODO: To implement
     } else {
       throw new Error(`Invalid parameter: ${this.type}`);
     }
